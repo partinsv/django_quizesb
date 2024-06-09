5f3ac459027a05a58267be08f5c2477c3bc35809
лександр Партин <partinsv@yandex.ru>
2024-06-09 23:27:17 +0500

    добавил отобрадение как на сайте точки

--- quiz_site/urls.py
+++ quiz_site/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quizzes.urls')),  # Корневой URL перенаправляется на приложение quizzes
+    path('', include('quizzes.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
