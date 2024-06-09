134f35410883853a3c833393e1b8fc0ffe34e564
лександр Партин <partinsv@yandex.ru>
2024-06-10 01:03:55 +0500

    добавь возможность в Django administration сразу добавлять Questions в Quizs

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Quiz, Question
 
-admin.site.register(Quiz)
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1  # Количество пустых вопросов для добавления
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
