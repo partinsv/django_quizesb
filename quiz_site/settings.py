5b5aedfef6bedad52fab025447124a40e6902b56
лександр Партин <partinsv@yandex.ru>
2024-06-12 11:39:31 +0500

    сделал новое оформление, добавил папку acciunts

--- quiz_site/settings.py
+++ quiz_site/settings.py
@@ -106,5 +106,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = 'profile'
-LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- quiz_site/urls.py
+++ quiz_site/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
 from quizzes import views as quiz_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quizzes.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('accounts/', include('accounts.urls')),  # Добавлено
     path('register/', quiz_views.register, name='register'),
     path('profile/', quiz_views.profile, name='profile'),
+    path('activity_log/', quiz_views.activity_log, name='activity_log'),
 ]
